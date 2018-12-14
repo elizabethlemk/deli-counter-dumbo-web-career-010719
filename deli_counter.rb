36884ddc9c08ca770cec712dcd086ddfbe201885
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-12-14 19:01:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+      array.each do |name|
+        puts "The line is currently: #{array.index(name) + 1}. #{name}"
+      end
+  end
+end
